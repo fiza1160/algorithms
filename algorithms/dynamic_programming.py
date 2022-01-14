38a3076c4b1dbe218f323e924df3580860e8249b
Liubov Murzina <fiza1160@gmail.com>
2022-01-14 19:09:45 +0300

    Add tasks for calculating the number of trajectories

--- algorithms/dynamic_programming.py
+++ algorithms/dynamic_programming.py
@@ -1,3 +1,6 @@
+from typing import Set
+
+
 def fibonacci(n: int) -> int:
     """
     Returns n-th Fibonacci number
@@ -24,6 +27,97 @@ def fibonacci(n: int) -> int:
     for i in range(2, n + 1):
         fib[i] = fib[i - 1] + fib[i - 2]
     return fib[n]
+
+
+def counting_trajectories(n: int) -> int:
+    """
+    The Grasshopper is in position 1.
+    The Grasshopper may jump to +1, +2 or +3.
+    How many possible trajectories does the grasshopper have to get to position n?
+    If n<=1, consider that the Grasshopper has 0 possible trajectory.
+
+    >>> counting_trajectories(0)
+    0
+    >>> counting_trajectories(1)
+    0
+    >>> counting_trajectories(2)
+    1
+    >>> counting_trajectories(3)
+    2
+    >>> counting_trajectories(4)
+    3
+    >>> counting_trajectories(7)
+    20
+    >>> counting_trajectories(-3)
+    0
+    """
+    if n <= 1:
+        return 0
+
+    trajectories = [0, 0, 1, 2, 3] + [0] * (n - 4)
+
+    for i in range(5, n + 1):
+        trajectories[i] = trajectories[i - 1] + trajectories[i - 2] + trajectories[i - 3]
+
+    return trajectories[n]
+
+
+def counting_trajectories_with_forbidden_cells(n: int, forbidden_cells: Set[int]) -> int:
+    """
+    The Grasshopper is in position 1.
+    The Grasshopper may jump to +1, +2 or +3.
+    The function receives a set of numbers of cells that cannot be jumped.
+    How many possible trajectories does the grasshopper have to get to position n?
+    If n<=1, consider that the Grasshopper has 0 possible trajectory.
+    If 1 is forbidden, consider that the Grasshopper has 0 possible trajectory.
+
+    >>> counting_trajectories_with_forbidden_cells(0, set())
+    0
+    >>> counting_trajectories_with_forbidden_cells(1, set())
+    0
+    >>> counting_trajectories_with_forbidden_cells(2, set())
+    1
+    >>> counting_trajectories_with_forbidden_cells(3, set())
+    2
+    >>> counting_trajectories_with_forbidden_cells(4, set())
+    3
+    >>> counting_trajectories_with_forbidden_cells(4, {2})
+    2
+    >>> counting_trajectories_with_forbidden_cells(4, {3})
+    2
+    >>> counting_trajectories_with_forbidden_cells(4, {4})
+    0
+    >>> counting_trajectories_with_forbidden_cells(9, {2,6,7})
+    3
+    >>> counting_trajectories_with_forbidden_cells(12, {3,6,7,10})
+    9
+    >>> counting_trajectories_with_forbidden_cells(8, {5})
+    13
+    >>> counting_trajectories_with_forbidden_cells(8, {1})
+    0
+    >>> counting_trajectories_with_forbidden_cells(-3, set())
+    0
+    """
+    if n <= 1 or 1 in forbidden_cells:
+        return 0
+
+    trajectories = [0] * 5
+    for i in range(2, 5):
+        if i not in forbidden_cells:
+            for k in range(i-1, 0, -1):
+                if k not in forbidden_cells:
+                    trajectories[i] = trajectories[k]+1
+                    break
+
+    trajectories += [0] * (n - 4)
+
+    for i in range(5, n + 1):
+        if i in forbidden_cells:
+            trajectories[i] = 0
+        else:
+            trajectories[i] = trajectories[i - 1] + trajectories[i - 2] + trajectories[i - 3]
+
+    return trajectories[n]
 
 
 if __name__ == "__main__":
